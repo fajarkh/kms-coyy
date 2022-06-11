b69daf55cd67c3c728729440897d81452a32f27c
fajarkh <fajarkh9@gmail.com>
2022-06-11 19:38:27 +0800

    untuk test copy ke coyy

---
+++ resources/views/components/image-marker.blade.php
@@ -0,0 +1,5 @@
+<div class="form-group">
+    <img src="{{ asset('lte/dist/img/photo3.jpg') }}" width="500" height="500" alt="tempat ttd"
+        style="border: 1px solid #c2c2c2;" id="imgmarker">
+    {{ Form::hidden('implentasi') }}
+</div>
